b9a2881329947de9eb4cebcfe011e1d1889885ab
Sam Morgan <s_morgan@me.com>
2017-02-24 12:44:42 +0000

    update to Mockup

--- mockup/scripts/scripts.js
+++ mockup/scripts/scripts.js
@@ -1,16 +1,15 @@
-var Spine = function(title, author, logo) {
-  this.BASE_SPINE_WIDTH = "30vw"
-  this.title = title;
-  this.author = author;
-  this.logo = logo;
-};
-
-Spine.prototype.initialize = function() {
-  this.width = Math.random() * this.BASE_SPINE_WIDTH;
-};
-
+var quotebox = document.getElementById('quotebox');
 var spines = document.querySelectorAll('.spine');
 
 for (var i = spines.length - 1; i >= 0; i--) {
-  spines[i].width = 30;
+  spines[i].onmouseenter = function() {
+    this.classList.add("spine--active");
+    var quote = this.getAttribute('data-quote');
+    if(quote) { quotebox.innerText = '"' + quote + '"'; };
+  }
+
+  spines[i].onmouseleave = function() {
+    this.classList.remove('spine--active');
+    quotebox.innerText = ""
+  }
 }
